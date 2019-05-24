7cd0ee1167c145e86a931c8aff2e08b335f704cd
kirinnsan <hirosi.t822@gmail.com>
2019-05-25 00:13:31 +0900

    全メニューのスクレイピング対応

--- src/main/java/com/example/demo/ScrapingController.java
+++ src/main/java/com/example/demo/ScrapingController.java
@@ -4,7 +4,10 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -20,31 +23,67 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ScrapingController {
 
-	public static final String OUTPUT_FILE_PATH = "C:\\work_new\\demo\\menuName.csv";
-
 	/**
 	 * @return
 	 */
-	@RequestMapping("/yakitori")
+	@RequestMapping("/parse")
 	public String htmlPasrse() {
 
+		//項目名のリストを作成
+		List<String> menuList = new ArrayList<String>(
+				Arrays.asList(settingConst.FAIR, settingConst.KANDOU, settingConst.YAKITORI, settingConst.IPPIN,
+						settingConst.SARADA, settingConst.GOHAN_DESSERT, settingConst.DRINK));
+
+		//出力パスのリストを作成
+		List<String> outputPathList = new ArrayList<String>(
+				Arrays.asList(settingConst.FAIR＿PATH, settingConst.KANDOU＿PATH,
+						settingConst.YAKITORI＿PATH, settingConst.IPPIN＿PATH, settingConst.SARADA＿PATH,
+						settingConst.GOHAN_DESSERT＿PATH, settingConst.DRINK＿PATH));
+
 		try {
-			//鳥貴族の焼き鳥メニューページをパース
-			Document document = Jsoup.connect("https://www.torikizoku.co.jp/menu/yakitori").get();
-			//メニュー名のみ取り出す
-			Elements elements = document.select("li h5");
 
-			//メニュー名を格納するリスト生成
-			List<String> menuNameList = new ArrayList<String>();
+			//パースするページのリスト用意
+			List<Document> ParseDocumentList = new ArrayList<Document>();
 
-			//メニュー名をリストに追加
-			for (Element element : elements) {
-				menuNameList.add(element.text());
-				System.out.println(element.text());
+			//メニューページをパース
+			ParseDocumentList.add(Jsoup.connect(settingConst.FAIR_MENU_URL).get());
+			ParseDocumentList.add(Jsoup.connect(settingConst.KANDOU_MENU_URL).get());
+			ParseDocumentList.add(Jsoup.connect(settingConst.YAKITORI_MENU_URL).get());
+			ParseDocumentList.add(Jsoup.connect(settingConst.IPPIN_MENU_URL).get());
+			ParseDocumentList.add(Jsoup.connect(settingConst.SARADA_MENU_URL).get());
+			ParseDocumentList.add(Jsoup.connect(settingConst.GOHAN_DESSERT_MENU_URL).get());
+			ParseDocumentList.add(Jsoup.connect(settingConst.DRINK_MENU_URL).get());
+
+			//項目名と項目毎メニューのマップを生成
+			Map<String, Document> menuMap = new LinkedHashMap<String, Document>();
+
+			for (int i = 0; i < menuList.size(); i++) {
+				menuMap.put(menuList.get(i), (ParseDocumentList.get(i)));
 			}
 
-			//メニュー名をフィルに出力
-			outputMenuName(OUTPUT_FILE_PATH, menuNameList);
+			int count = 0;
+
+			for (String key : menuMap.keySet()) {
+
+				//メニュー名を格納するリスト生成
+				List<String> menuNameList = new ArrayList<String>();
+
+
+				//ページの中からメニュー名のみ取り出す
+				Elements elements = menuMap.get(key).select("li h5");
+
+				//メニュー名をリストに追加
+				for (Element element : elements) {
+					menuNameList.add(element.text());
+					System.out.println(element.text());
+				}
+
+				//メニュー名をファイルに出力
+				outputMenuName(key,outputPathList.get(count), menuNameList);
+
+				count++;
+			}
+
 
 			//System.out.println(document.html());
 		} catch (IOException e) {
@@ -58,7 +97,7 @@ public class ScrapingController {
 	/**
 	 *
 	 */
-	private void outputMenuName(String path, List<String> menuNameList) {
+	private void outputMenuName(String key, String path, List<String> menuNameList) {
 		FileWriter fw = null;
 		BufferedWriter bf = null;
 		try {
@@ -68,7 +107,7 @@ public class ScrapingController {
 			if (fw != null) {
 				StringBuilder sb = new StringBuilder();
 				for (String menuName : menuNameList) {
-					sb.append("焼鳥").append(",").append(menuName).append("\n");
+					sb.append(key).append(",").append(menuName).append("\n");
 				}
 				//メニュー名をファイルに書き込み
 				bf.write(sb.toString());
@@ -79,10 +118,10 @@ public class ScrapingController {
 			e.printStackTrace();
 		} finally {
 			try {
-				if(bf != null) {
+				if (bf != null) {
 					bf.close();
 				}
-				if(fw != null) {
+				if (fw != null) {
 					fw.close();
 				}
 			} catch (IOException e) {
---
+++ src/main/java/com/example/demo/settingConst.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+public class settingConst {
+
+	//期間限定メニューURL
+	public static final String FAIR_MENU_URL = "https://www.torikizoku.co.jp/menu/fair";
+	//感動メニューURL
+	public static final String KANDOU_MENU_URL = "https://www.torikizoku.co.jp/menu/kandou";
+	//焼鳥メニューURL
+	public static final String YAKITORI_MENU_URL = "https://www.torikizoku.co.jp/menu/fair";
+	//逸品メニューURL
+	public static final String IPPIN_MENU_URL = "https://www.torikizoku.co.jp/menu/ippin";
+	//スピードメニューURL
+	public static final String SARADA_MENU_URL = "https://www.torikizoku.co.jp/menu/sarada";
+	//ご飯もの_デザートメニューURL
+	public static final String GOHAN_DESSERT_MENU_URL = "https://www.torikizoku.co.jp/menu/gohan";
+	//ドリンクメニューURL
+	public static final String DRINK_MENU_URL = "https://www.torikizoku.co.jp/menu/drink";
+
+	//項目名
+	//期間限定
+	public static final String FAIR = "期間限定";
+	//感動
+	public static final String KANDOU = "感動";
+	//焼鳥
+	public static final String YAKITORI = "焼鳥";
+	//焼鳥
+	public static final String IPPIN = "逸品";
+	//焼鳥
+	public static final String SARADA = "スピード";
+	//焼鳥
+	public static final String GOHAN_DESSERT = "ご飯もの_デザート";
+	//焼鳥
+	public static final String DRINK = "ドリンク";
+
+	//出力パス
+	//期間限定
+	public static final String FAIR＿PATH = "C:\\work_new\\scraping_csvDownload\\FAIR.csv";
+	//感動
+	public static final String KANDOU＿PATH = "C:\\work_new\\scraping_csvDownload\\KANDOU.csv";
+	//焼鳥
+	public static final String YAKITORI＿PATH = "C:\\work_new\\scraping_csvDownload\\YAKITORI.csv";
+	//焼鳥
+	public static final String IPPIN＿PATH = "C:\\work_new\\scraping_csvDownload\\IPPIN.csv";
+	//焼鳥
+	public static final String SARADA＿PATH = "C:\\work_new\\scraping_csvDownload\\SARADA.csv";
+	//焼鳥
+	public static final String GOHAN_DESSERT＿PATH = "C:\\work_new\\scraping_csvDownload\\GOHAN_DESSERT.csv";
+	//焼鳥
+	public static final String DRINK＿PATH = "C:\\work_new\\scraping_csvDownload\\DRINK.csv";
+}
